0455099bd19d4d2db6075458f8458618f93cd020
krushanu <mohapatrakrushanu@gmail.com>
2018-01-11 14:59:11 +0530

    Twitter Auth genetator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'twitter Integration';
+
+  constructor(private _http: Http) {}
+  getAuth() {
+    console.log('authenticate !!!');
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+    this._http.post('http://localhost:3000/authorize', { headers: headers }).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
